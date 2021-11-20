e9facafad86ae0de76118772aab7aba8ee90b538
Vamsi Perumallu Yalamanchili <vamsiperumallu@gmail.com>
2021-11-20 11:25:44 -0600

    Update and rename Food_Diet.txt to Food_Diet.py

--- Food_Diet.py
+++ Food_Diet.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 10 19:00:19 2021
+Problem:
+1.	Formulate an optimization model (a linear program) to find the cheapest diet that satisfies the maximum and minimum daily nutrition constraints, and solve it using PuLP.  Turn in your code and the solution. (The optimal solution should be a diet of air-popped popcorn, poached eggs, oranges, raw iceberg lettuce, raw celery, and frozen broccoli. UGH!)
+2.	Please add to your model the following constraints (which might require adding more variables) and solve the new model:
+    a.	If a food is selected, then a minimum of 1/10 serving must be chosen. (Hint: now you will need two variables for each food i: whether it is chosen, and how much is part of the diet. You’ll also need to write a constraint to link them.)
+    b.	Many people dislike celery and frozen broccoli. So at most one, but not both, can be selected.
+    c.	To get day-to-day variety in protein, at least 3 kinds of meat/poultry/fish/eggs must be selected. [If something is ambiguous (e.g., should bean-and-bacon soup be considered meat?), just call it whatever you think is appropriate – I want you to learn how to write this type of constraint, but I don’t really care whether we agree on how to classify foods!]
+
 
 @author: xxxxx
 
